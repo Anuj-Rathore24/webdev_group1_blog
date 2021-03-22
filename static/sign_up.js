7fa499801c6bb0033bf085e981a5772e747bd0ab
Anuj-Rathore24 <80690679+Anuj-Rathore24@users.noreply.github.com>
2021-03-22 23:38:32 +0530

    Create sign_up.js

---
+++ static/sign_up.js
@@ -0,0 +1,25 @@
+window.onload=function(){
+    var submit_form = document.getElementById("Form");
+    submit_form.onsubmit = function (e) {
+        e.preventDefault();
+        console.log(e.target.confirm_password.value) 
+        console.log(e.target.password.value) 
+        if(e.target.confirm_password.value==e.target.password.value){
+            validateForm(e.target);
+        }else{
+            alert("Incorrect password")
+        }
+  };
+    validateForm=function(form){
+        var pass_element = form.password.value;
+        var email_element = form.email.value;
+        var Last_name = form.Last_name.value;
+        var First_name = form.First_name.value;
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "http://localhost:80/sign_up_credentials", true);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        var Credentials = { email: email_element, password: pass_element, l_name: Last_name, f_name: First_name };
+        xhttp.send(JSON.stringify(Credentials))
+
+    }
+}
