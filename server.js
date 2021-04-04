547efc59bdeaa7220142c74a925855978fbf613f
Anuj-Rathore24 <anujsingh2409@gmail.com>
2021-04-05 01:00:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,22 @@ var con = createConnection({
 
 con.query("USE blog_website");
 
+// login-session setup
+var session = require("express-session");
+app.use(
+  session({
+    name: "Session_Id",
+    saveUninitialized: false,
+    resave: false,
+    resave: false,
+    secret: "auth",
+    Cookie: {
+      sameSite: true,
+      secure: false,
+    },
+  })
+);
+
 app.use("/static", express.static("static")); //for serving static files
 app.use(express.static(path.join(__dirname, "public")));
 app.use(urlencoded());
@@ -40,27 +56,46 @@ app.set("view engine", "html");
 app.get("/", (req, res) => {
   res.status(200).render("home.html");
 });
-app.get("/sign_up", (req, res) => {
+
+
+var redirecthome = (req, res, next) => {
+  if (req.session.userId) {
+    res.redirect("/");
+  } else {
+    next();
+  }
+};
+var redirectlogin = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+app.get("/my_blogs", redirectlogin,(req, res) => {
+  res.status(200).render("about_user.html");
+});
+app.get("/sign_up", redirecthome, (req, res) => {
   res.status(200).render("sign_up.html");
 });
 
 var check = { correct: "" };
-app.get("/login", (req, res) => {
+app.get("/login", redirecthome, (req, res) => {
   res.status(200).render("index.html");
   check.correct = "";
 });
-app.get("/posting", (req,res)=>{
-  res.status(200).render("posting.html")
-})
+app.get("/posting", redirectlogin,(req, res) => {
+  res.status(200).render("posting.html");
+});
 
-app.get("/display",(req,res)=>{
-  res.status(200).render("display.html")
-})
+app.get("/display", (req, res) => {
+  res.status(200).render("display.html");
+});
 
 // checking login credentails
 app.post("/login_credentials", (req, res) => {
   var Body = req.body;
-  console.log(Body);
   email = Body.email;
   pswd = Body.password;
 
@@ -74,6 +109,7 @@ app.post("/login_credentials", (req, res) => {
       } else {
         if (result[0].Password == pswd) {
           check.correct = "correct";
+          req.session.userId = email;
           res.send(check);
         } else {
           check.correct = "wrong";
@@ -96,15 +132,51 @@ app.post("/sign_up_credentials", (req, res) => {
     }
   );
 });
-var car=""
-app.post("/publish_credentials",(req,res)=>{
-  console.log("working")
-  var Body=req.body
+var car = "";
+app.post("/publish_credentials", (req, res) => {
+  console.log("working");
+  var Body = req.body;
+  con.query(
+    `insert into user_blog_database values("${req.session.userId}",'${JSON.stringify(Body.content)}')`,
+    function (err, result) {
+      if (err) throw err;
+      car = Body;
+    }
+  );
+  console.log(Body);
+});
+app.post("/view_myblog", (req, res) => {
+  console.log("working");
+  var Body = req.body;
   car=Body
-  console.log(Body)
 })
-app.post("/blog_display",(req,res)=>{
-  res.send(car)
+app.post("/blog_display", redirectlogin,(req, res) => {
+  res.send(car);
+});
+var user_credentials = {};
+app.post("/user_information", (req, res) => {
+  if (!req.session.userId) {
+    res.send(user_credentials);
+  } else {
+    con.query(
+      `select Name, Last_Name from user_database where Email="${req.session.userId}"`,
+      function (err, result) {
+        if (err) throw error;
+        console.log(result);
+        user_credentials.Email = req.session.userId;
+        user_credentials.Name = result[0].Name;
+        user_credentials.Last_Name = result[0].Last_Name;
+        console.log(user_credentials);
+        res.send(user_credentials);
+      }
+    );
+  }
+});
+app.post("/blogs_information",(req,res)=>{
+  con.query(`select content from user_blog_database where Email_Id="${req.session.userId}"`,function(err,result){
+    if(err) throw error;
+    res.send(result)
+  })
 })
 
 // This function listen to every activity when called
